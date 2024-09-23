bb8d2f59405d789a776eee3074c1ebd8f1a7b033
katerynalanina <lanina.kateryna@gmail.com>
2024-09-23 17:55:50 +0300

    fix: TS errors

--- src/components/common/Container/Container.tsx
+++ src/components/common/Container/Container.tsx
@@ -1,9 +1,12 @@
 import styles from "./container.module.css"
 import classNames from 'classnames';
-import {forwardRef} from "react";
+import {forwardRef, HTMLAttributes} from "react";
 
+interface ContainerProps extends HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
 
-const Container = forwardRef(({ children, className, ...rest }, ref) => {
+const Container = forwardRef<HTMLDivElement, ContainerProps>(({ children, className, ...rest }, ref) => {
   return (
     <div ref={ref} {...rest} className={classNames(styles.container, className)}>
       {children}
