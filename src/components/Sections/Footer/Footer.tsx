3c7dc656b98c435278b9173a2ae226cae05bc3f1
katerynalanina <lanina.kateryna@gmail.com>
2024-09-23 17:11:12 +0300

    feat: footer section

--- src/components/Sections/Footer/Footer.tsx
+++ src/components/Sections/Footer/Footer.tsx
@@ -1,9 +1,48 @@
+import styles from "./footer.module.css";
+import Container from "../../common/Container/Container.tsx";
+import Input from "../../common/Input/Input.tsx";
+import Button from "../../UI/Button/Button.tsx";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer className={styles["footer"]}>
+      <Container className={styles["footer-container"]}>
+        <div>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Connect</a></li>
+          </ul>
+        </div>
+        <div className={styles["footer-logo"]}>
+          <img src="/src/assets/logo.png" alt="" width={200} height={130}/>
+        </div>
+        <div className={styles["footer-subscribe"]}>
+          <h3>JOIN THE JUST DO IT MOVEMENT</h3>
+          <p>to receive your weekly positivity email.</p>
+          <form action="" className={styles["form"]}>
+            <Input
+              label={"First Name"}
+              type="text"
+              classes={{
+                label: styles["form-label"]
+              }}
+              placeholder={"Enter name"}
+            />
+            <Input
+              label={"Email"}
+              type="email"
+              classes={{
+                label: styles["form-label"]
+              }}
+              placeholder={"Enter email"}
+            />
+            <Button type="submit" buttonStyle="primary">Submit</Button>
+          </form>
+        </div>
+      </Container>
+      <div className={styles["copyrights"]}>© John Doe. All Rights Reserved</div>
+    </footer>
   );
 };
 
