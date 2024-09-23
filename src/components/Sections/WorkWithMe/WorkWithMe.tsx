f174b2857be98b0194aa9816dd7478aff5626bc1
katerynalanina <lanina.kateryna@gmail.com>
2024-09-23 13:04:04 +0300

    feat: work with me section

--- src/components/Sections/WorkWithMe/WorkWithMe.tsx
+++ src/components/Sections/WorkWithMe/WorkWithMe.tsx
@@ -1,9 +1,25 @@
+import styles from "./styles.module.css";
+import {useRef} from "react";
+import {useParallaxAnimation} from "../../../hooks/useParalaxAnimation.ts";
+import Container from "../../common/Container/Container.tsx";
+import {useGSAP} from "@gsap/react";
+import {elementAppearingEffect} from "../../../utils/animations/animations.ts";
 
 const WorkWithMe = () => {
+  const imgRef = useParallaxAnimation();
+  const textRef = useRef<HTMLDivElement | null>(null);
+
+  useGSAP(()=> {
+    elementAppearingEffect(textRef?.current)
+  }, [textRef])
+
   return (
-    <div>
-      WorkWithMe
-    </div>
+    <section className={styles["work"]} ref={imgRef}>
+      <Container ref={textRef}>
+        <h2 className={styles["work-title"]}>Leveraging my personal experience and the <br/> <span>Just Do It Global framework</span></h2>
+        <p className={styles["work-text"]}>I help organizations build compassion, competence, and accountability in the workplace. As a result, sustainable and joyful peak productivity, engagement, and satisfaction become the new norm.</p>
+      </Container>
+    </section>
   );
 };
 
